f1aef7914ccc920ce1fc400973199247abfb05f7
915448715@qq.com <915448715@qq.com>
2017-08-01 14:29:44 +0800

    add get_union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+	var result = collection_a;
+	var c = 0;
+	for(var i = 0;i<collection_b.length;i++){
+		c = 0;
+		for(var j = 0;j<collection_a.length;j++){
+			if(collection_b[i] === collection_a[j]){
+				c = 1;
+			}
+		}
+		if(c !== 1){
+			result.push(collection_b[i]);
+		}
+	}
+	return result;
 }
-
+var collection_a = [10, 27, 28, 19, 5];
+  var collection_b = [5, 78, 28, 19, 23];
+  console.log(get_union(collection_a, collection_b));
 module.exports = get_union;
 
