4a061442e4a6daed58a142ce6be81b8fb0220728
915448715@qq.com <915448715@qq.com>
2017-08-01 13:49:37 +0800

    add get_integer_interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+	var result = [];
+	if(number_a>number_b){
+		for(var i = 0,a_b = number_a - number_b + 1;i<a_b;i++){
+			result.push(number_a - i);  
+		}
+	}else if(number_a<number_b){
+		for(var i = 0,b_a = number_b - number_a + 1;i<b_a;i++){
+			result.push(number_a + i);  
+		}
+	}else{
+		result.push(number_a);
+	}
+	return result;
 }
-
 module.exports = get_integer_interval;
-
