e945d8d2896796f7e1a97b5aa41fe1ce2ba73741
915448715@qq.com <915448715@qq.com>
2017-08-01 22:58:06 +0800

    add ruduce

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+	var NUMBER = 10*number;
+	var INTERVAL = 10*interval;
+	var res = [];
+	for(var i = 0;i < (NUMBER / INTERVAL + 1);i++){
+			res.push(NUMBER - i*INTERVAL);
+		}
+	return res.map(function(x){
+		return x/10;
+	});
 }
-
 module.exports = spilt_to_zero;
