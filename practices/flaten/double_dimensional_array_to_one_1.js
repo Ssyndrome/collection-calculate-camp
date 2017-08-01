2ce10e1b71f8f2e49c5989a277a15b90d234ec28
915448715@qq.com <915448715@qq.com>
2017-08-01 15:53:44 +0800

    add 2 files from flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,10 @@
 'use strict';
 
-function double_to_one(collection) {
-
-  //在这里写入代码
+function double_to_one(arr) {
+	var arr1 = (arr + '').split(',');//将数组转字符串后再以逗号分隔转为数组
+        var arr2 = arr1.map(function(x){
+            return Number(x);
+        });
+        return arr2;
 }
-
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+	var unique = (collection + '').split(',');
+	var unique_ = unique.map(function(x){
+		return Number(x);
+	});
+	var c = 0;
+	var res = [unique_[0]];
+	unique_.forEach(function(val){
+		c = 0;
+		res.forEach(function(val_r){
+			if(val_r === val){
+				c = 1;
+			}
+		});
+		if(c === 0){
+			res.push(val);
+		}
+	});
+	return res;
 }
 
 module.exports = double_to_one;
