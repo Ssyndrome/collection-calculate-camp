74f22045ebd2c4dce8b0deb08f16007f27d3b40a
915448715@qq.com <915448715@qq.com>
2017-08-01 21:50:41 +0800

    add 2 files from reduce

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,15 @@
 'use strict';
 
-function compute_chain_median(collection) {
-  //在这里写入代码
+function compute_chain_median(chain) {
+	var res = chain.split('->').map(function(i){return Number(i);}).sort(function(a,b){
+		if(a>b){
+			return 1;
+		}else if(a<b){
+			return -1;
+		}else{
+			return 0;
+		}
+	});
+	return (res[res.length/ 2]+res[ res.length / 2 -  1])/2;
 }
-
 module.exports = compute_chain_median;
